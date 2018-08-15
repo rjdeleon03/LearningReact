80719c7dea321adf058c53f8a0019befbad7edb5
pabs <pabs@alink-group.co.jp>
2018-08-16 06:43:26 +0800

    Render conditionals and lists

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,16 +2,37 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>No tags found.</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.state.tags.length === 0 && "Please create a new tag."}
+        {this.renderTags()}
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
