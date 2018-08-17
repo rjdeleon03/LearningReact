a7db4d40801d34300361aa726cf8eac95cab1e04
pabs <pabs@alink-group.co.jp>
2018-08-18 06:08:41 +0800

    Implement incrementation

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -18,11 +18,20 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement() {
+    this.state.count++;
+  }
+
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.state.tags.length === 0 && "Please create a new tag."}
         {this.renderTags()}
       </React.Fragment>
